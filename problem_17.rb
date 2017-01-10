168da5b3163344a0cfa2541dc2111bd2d3c91aa0
Zach <zacharymichaelmoore@gmail.com>
2017-01-09 20:01:16 -0500

    added comments

--- problem_17.rb
+++ problem_17.rb
@@ -1,5 +1,9 @@
 require 'humanize'
 
+# convert a range to an array and map over it
+# map is passed a block as an argument that calls the humanize library
+# the result is joined and spaces and dashes are removed
+# finally size counts the number of characters (alt word for length)
 def number_letter_counts?
   (1..1000).to_a.map(&:humanize).join.tr(" -", "").size
 end
